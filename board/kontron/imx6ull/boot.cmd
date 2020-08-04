73cccef602cba38d6235c860e7e62101266dad7a
Florian Mayer <florianmayer@posteo.net>
2020-08-04 12:56:08 +0200

    add custom device tree

--- board/kontron/imx6ull/boot.cmd
+++ board/kontron/imx6ull/boot.cmd
@@ -1,5 +1,5 @@
 mmc dev 0
 fatload mmc 0:1 ${loadaddr} zImage
-fatload mmc 0:1 ${fdt_addr_r} imx6ull-kontron-n6411-s.dtb
+fatload mmc 0:1 ${fdt_addr_r} baseboard.dtb
 setenv bootargs root=/dev/mmcblk0p2 rootwait
 bootz ${loadaddr} - ${fdt_addr_r}
